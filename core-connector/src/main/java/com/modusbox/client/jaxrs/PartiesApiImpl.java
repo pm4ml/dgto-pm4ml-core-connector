e92fc318345fffcf1cf7efc6bb3104396b6ff8a1
Juan Correa <gibaros@users.noreply.github.com>
2021-09-28 16:01:53 -0400

    Update jaxrs class after swagger update

--- core-connector/src/main/java/com/modusbox/client/jaxrs/PartiesApiImpl.java
+++ core-connector/src/main/java/com/modusbox/client/jaxrs/PartiesApiImpl.java
@@ -3,12 +3,15 @@ package com.modusbox.client.jaxrs;
 import com.modusbox.client.api.PartiesApi;
 import com.modusbox.client.model.TransferPartyInbound;
 
-import javax.validation.constraints.Size;
-
 public class PartiesApiImpl implements PartiesApi {
 
     @Override
-    public TransferPartyInbound getPartiesByIdTypeIdValue(String idType, @Size(min = 1, max = 128) String idValue) {
+    public TransferPartyInbound getPartiesByIdTypeIdValue(String idType, String idValue) {
+        return null;
+    }
+
+    @Override
+    public TransferPartyInbound getPartiesByIdTypeIdValueIdSubValue(String idType, String idValue, String idSubValue) {
         return null;
     }
 }
